b380af5185c8e37f5c7d19bb6a66c04b93ef597e
Harry Gustave <harry23gust@gmail.com>
2021-03-23 20:24:20 -0500

    added script.js variables

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,15 @@ var getUserRepos = function (user) {
   });
 });
 }
+
+var formSubmithandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username")
+    }
+    }

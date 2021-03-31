66e562553d5b02dcdbbf6423d261267f50c7a0fc
Matt Lemke <mattrlemke@gmail.com>
2021-03-31 12:00:57 -0500

    pseudo coding added and documents formatted

--- assets/script.js
+++ assets/script.js
@@ -10,97 +10,97 @@ let userScore = 0;
 let click = document.getElementById("action")
 
 // Next Button On Click
-document.getElementById("next-btn").onclick = function(){
-time = 3
-document.getElementById("word").innerHTML = ""
-document.getElementById("definition").innerHTML = ""
-document.getElementById("spell").value = ""
-document.getElementById("time-left").style.display = "block"
-getWord()
+document.getElementById("next-btn").onclick = function () {
+  time = 3
+  document.getElementById("word").innerHTML = ""
+  document.getElementById("definition").innerHTML = ""
+  document.getElementById("spell").value = ""
+  document.getElementById("time-left").style.display = "block"
+  getWord()
 }
 
 // Get Word Function 
-let getWord = function() {
-fetch("https://random-words-with-pronunciation.p.rapidapi.com/word", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "630b6ad385mshad49248fe7e32e3p1d76e3jsnf0bd5377f502",
-		"x-rapidapi-host": "random-words-with-pronunciation.p.rapidapi.com"
-	}
-})
-.then(function(response){
-    response.json().then(function(data){
-      
-      // Change Views
-      document.getElementById("start-game").style.display = "none"
-      document.getElementById("countdown").style.display = "block"
-      document.getElementById("gamebox").style.display = "block"
-      
-      // 3 Second Timer
-      time = 3;
-
-      timer = setInterval (function function1() {
-  
-        document.getElementById("inner_circle").innerHTML = time + "&nbsp";
-        // console.log(time)
-        time -= 1
-
-        if(time < 0 ) {
-
-        //  WORD APPEARS!
-
-          clearInterval(timer)
-          document.getElementById("inner_circle").innerHTML = ""
-          document.getElementById("countdown").style.display = "none"
-          document.getElementById("time-left").style.display = "none"
-          
-         // Get Values
-          word = data[0].word
-          definition = data[0].definition
-          pronunciation = data[0].pronunciation
-          console.log(word)
-        
-        appear = document.getElementById("word")
-
-        //  console.log(appear)
-        appear.innerHTML = word
-       
-
-         setTimeout(function() {
-             document.getElementById("word").innerHTML = ""
-             
-           
-         }, 1000)
-        }
-      }, 1000)
+let getWord = function () {
+  fetch("https://random-words-with-pronunciation.p.rapidapi.com/word", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "630b6ad385mshad49248fe7e32e3p1d76e3jsnf0bd5377f502",
+      "x-rapidapi-host": "random-words-with-pronunciation.p.rapidapi.com"
+    }
   })
- })
-.catch(err => {
-	console.error(err);
-});
+    .then(function (response) {
+      response.json().then(function (data) {
+
+        // Change Views
+        document.getElementById("start-game").style.display = "none"
+        document.getElementById("countdown").style.display = "block"
+        document.getElementById("gamebox").style.display = "block"
+
+        // 3 Second Timer
+        time = 3;
+
+        timer = setInterval(function function1() {
+
+          document.getElementById("inner_circle").innerHTML = time + "&nbsp";
+          // console.log(time)
+          time -= 1
+
+          if (time < 0) {
+
+            //  WORD APPEARS!
+
+            clearInterval(timer)
+            document.getElementById("inner_circle").innerHTML = ""
+            document.getElementById("countdown").style.display = "none"
+            document.getElementById("time-left").style.display = "none"
+
+            // Get Values
+            word = data[0].word
+            definition = data[0].definition
+            pronunciation = data[0].pronunciation
+            console.log(word)
+
+            appear = document.getElementById("word")
+
+            //  console.log(appear)
+            appear.innerHTML = word
+
+
+            setTimeout(function () {
+              document.getElementById("word").innerHTML = ""
+
+
+            }, 1000)
+          }
+        }, 1000)
+      })
+    })
+    .catch(err => {
+      console.error(err);
+    });
 };
 
 // When Next Button Is Clicked
-document.getElementById("main-btn").onclick = function(event) {
-  
-  let results = document.getElementById("spell").value
-//   console.log(results)
-//  console.log(word)
+document.getElementById("main-btn").onclick = function (event) {
 
-// Word Comparison
-  if (word.toLowerCase() === results.toLowerCase()){
+  let results = document.getElementById("spell").value
+  //   console.log(results)
+  //  console.log(word)
+
+  // Word Comparison
+  if (word.toLowerCase() === results.toLowerCase()) {
 
     userScore += 1; //upgrading score value with 1
     document.getElementById("yourScore").innerHTML = "Your Score: " + userScore
-      console.log(userScore);
+    console.log(userScore);
     fetch(
       'https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN'
     )
       // Convert the response to JSON
-      .then(function(response) {
+      .then(function (response) {
         return response.json();
       })
-      .then(function(response) {
+      .then(function (response) {
         // console.log(response)
         let grabGiphy = document.getElementById("word")
 
@@ -117,36 +117,36 @@ document.getElementById("main-btn").onclick = function(event) {
         <p id="learn"> Pronunciation: ${pronunciation}</p>
         `
         grabDefinition.innerHTML = setDefinition
-      //  console.log(definition)
-      //  console.log(pronunciation)
+        //  console.log(definition)
+        //  console.log(pronunciation)
       });
-    
-      
-  // IF  word was wrong
-	}  else  {
-        userScore -= 1; //downgrade score value with 1
+
+
+    // IF  word was wrong
+  } else {
+    userScore -= 1; //downgrade score value with 1
 
     document.getElementById("yourScore").innerHTML = "Your Score: " + userScore
-  
-     let getX = document.getElementById("word")
-     
 
-     let setX = document.createElement("span")
-     setX.setAttribute("style", 'font-size:100px;')
-     setX.innerHTML = '&#10060'
-     getX.appendChild(setX)
+    let getX = document.getElementById("word")
 
-     let grabDefinition = document.getElementById("definition")
 
-        let setDefinition = `
+    let setX = document.createElement("span")
+    setX.setAttribute("style", 'font-size:100px;')
+    setX.innerHTML = '&#10060'
+    getX.appendChild(setX)
+
+    let grabDefinition = document.getElementById("definition")
+
+    let setDefinition = `
         <h3 id="learn"> Word: ${word} </h3>
         <p id="learn"> Definition: ${definition}</p>
         <p id="learn"> Pronunciation: ${pronunciation}</p>
         `
-        grabDefinition.innerHTML = setDefinition
-      }
-  
-    
+    grabDefinition.innerHTML = setDefinition
+  }
+
+
 }
 
 
@@ -161,24 +161,24 @@ function saveGame() {
 
   // Place values of High Score and Initials
 
-  highscore = document.getElementById("highScore").innerHTML 
-  
+  highscore = document.getElementById("highScore").innerHTML
+
 
   initials = document.getElementById("initials").value
 
- 
+
 
 
   let logScores = JSON.parse(localStorage.getItem("yourScores")) || [];
- 
-// push to array and save
-  let numbers = {Score: highscore} 
-   logScores.push(numbers)
+
+  // push to array and save
+  let numbers = { Score: highscore }
+  logScores.push(numbers)
   console.log(logScores)
-  
+
 
   setScore(logScores)
- 
+
 
   localStorage.setItem("yourScores", JSON.stringify(logScores))
 
@@ -186,26 +186,26 @@ function saveGame() {
 };
 
 // display scores
-  function setScore(logScores) {
+function setScore(logScores) {
 
   for (var i = 0; i < logScores.length; i++) {
-      var logScore = document.getElementById("finalLog")
-      var ulScores = document.createElement("li")
-      ulScores.setAttribute('class', 'yourScores')
-      ulScores.textContent = logScores[i].Score
+    var logScore = document.getElementById("finalLog")
+    var ulScores = document.createElement("li")
+    ulScores.setAttribute('class', 'yourScores')
+    ulScores.textContent = logScores[i].Score
 
-      finalLog.appendChild(ulScores)
+    finalLog.appendChild(ulScores)
 
-      var ulInitials = document.createElement('li')
-      ulInitials.setAttribute('class', 'yourInitials')
-      ulInitials.textContent = logScores[i].Initials;
+    var ulInitials = document.createElement('li')
+    ulInitials.setAttribute('class', 'yourInitials')
+    ulInitials.textContent = logScores[i].Initials;
 
-      finalLog.appendChild(ulInitials)
+    finalLog.appendChild(ulInitials)
 
-      
+
   };
-      
-  };
+
+};
 
 
 
@@ -222,7 +222,7 @@ function highscores() {
   setScore(getScore)
   console.log(getScore)
 
-  
+
 };
 
 
